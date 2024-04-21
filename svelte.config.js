3dc3a981e3ea6a9c47871505b00ab4c79087bf85
rue-22 <cssalces@up.edu.ph>
2024-04-21 23:01:21 +0800

    fix: edit svelte config file

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,10 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '404.html',
-			pages: 'build'
+			pages: 'build',
+			prerender: {
+				default: true
+			}
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
