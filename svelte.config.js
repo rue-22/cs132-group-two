e7ea4aba11c5080ce3f71a6cf9dfa13c903c2ecb
rue-22 <cssalces@up.edu.ph>
2024-04-21 23:15:38 +0800

    chore: edit svelte config file

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			appDir: 'app',
-			fallback: '404.html',
-			pages: 'build'
+			fallback: '404.html'
 		})
 	},
 	preprocess: vitePreprocess()
