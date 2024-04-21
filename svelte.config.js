0883fb2362889031117d265f6b903e9ee1f66761
rue-22 <cssalces@up.edu.ph>
2024-04-21 22:31:05 +0800

    fix: modify paths.base

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 			pages: 'build'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : 'cs-132-group-two'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	},
 	preprocess: vitePreprocess()
